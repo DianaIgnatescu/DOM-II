a93248cb85636cf3044f40c975f1860e4dee3290
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-01-29 13:24:14 +0000

    MVP: Added event listeners for keydown, focus, blur and dblclick

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
 window.addEventListener('load', () => {
 
 // 2. === Mouseover === change title image on mouseover.
+
   const titleImage = document.getElementById('title-image');
   titleImage.addEventListener('mouseover', () => {
     titleImage.src = 'img/destination.jpg';
@@ -15,17 +16,48 @@ window.addEventListener('load', () => {
   });
 
 // 4. === Keydown === go to contact link when you hit the c key.
-  
+
+  window.addEventListener('keydown', (event) => {
+    if (event.code === 'KeyC') {
+      window.location.href = '/contact';
+    }
+  });
 
 // 5. === Focus === change the color of the navigation item when in "focus".
 
-// 6. === Dblclick === open an image in new window when you double click on it.
+  const navs = document.querySelectorAll('.nav-link');
 
-// 7. === Scroll === Make header get smaller as you "scroll" down.
+    navs.forEach((item) => {
+      item.addEventListener('click', (event) => {
+        event.preventDefault();
+      });
+      item.addEventListener('focus', () => {
+        item.style.color = '#17A2B8';
+      });
 
-// 8. === Click === Click on sign me up to say alert 'everything is full'.
+// 6. === Blur === change the color of the navigation item when not in focus anymore 
+    item.addEventListener('blur', () => {
+      item.style.color = '#212529'; 
+    });
+  });
 
-// 9. === Contextmenu === right clicking on something removes it. 
+// 7. === Dblclick === open an image in new window when you double click on it.
 
-// 10. === Keypress === pressing space either takes you to the bottom or the top.
+  const adventure = document.getElementById('adventure');
+  adventure.addEventListener('dblclick', () => {
+    window.location.href = 'img/adventure.jpg';
+  });
+
+// 8. === Scroll === Make header get smaller as you "scroll" down.
+
+
+
+// 9. === Click === Click on sign me up to say alert 'everything is full'.
+
+// 10. === Contextmenu === right clicking on something removes it. 
+
+// 11. === Keypress === pressing space either takes you to the bottom or the top.
+
+
+
 });
