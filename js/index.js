a77554e09622a766cac1c2e976d578104fd6dd5b
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-01-29 12:22:34 +0000

    MVP: Listed the 10 event listeners to be created

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+// 1. === Load === wait for everything on load.
+
+// 2. === Mouseover === change title image on mouseover.
+
+// 3. === Mouseout === change title image back on mouseout.
+
+// 4. === Keydown === go to contact link when you hit the c key.
+
+// 5. === Focus === change the color of the navigation item when in "focus".
+
+// 6. === Dblclick === open an image in new window when you double click on it.
+
+// 7. === Scroll === Make header get smaller as you "scroll" down.
+
+// 8. === Click === Click on sign me up to say alert 'everything is full'.
+
+// 9. === Contextmenu === right clicking on something removes it. 
+
+// 10. === Keypress === pressing space either takes you to the bottom or the top.
